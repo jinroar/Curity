80d0d338c83d3ce1ae7f84b41049470b6b5340f3
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-09-05 22:41:28 +0800

    [MOD] rearrange files

--- app/src/main/java/com/example/curity/Login.kt
+++ app/src/main/java/com/example/curity/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.curity.MainActivity.HomePage
 import com.example.curity.SignUp.SignUpP1
 import com.example.curity.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
---
+++ app/src/main/java/com/example/curity/MainActivity/HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.curity.MainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.curity.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
