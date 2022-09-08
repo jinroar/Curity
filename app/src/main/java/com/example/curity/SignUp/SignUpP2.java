57ca8cb98236232742564cb834981327a02d1489
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-09-08 22:46:45 +0800

    [MOD] multiple choices contacts

--- app/src/main/java/com/example/curity/SignUp/SignUpP2.java
+++ app/src/main/java/com/example/curity/SignUp/SignUpP2.java
@@ -6,8 +6,6 @@ import androidx.cursoradapter.widget.SimpleCursorAdapter;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.curity.R;
@@ -16,7 +14,6 @@ public class SignUpP2 extends AppCompatActivity {
 
     Cursor cursor;
     ListView listView;
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +23,40 @@ public class SignUpP2 extends AppCompatActivity {
         // declaring listView using findViewById()
         listView = findViewById(R.id.ListView);
 
-        //Call for the Conatcts
+        // call for the Contacts
         getContacts();
     }
 
     public void getContacts() {
 
         // create cursor and query the data
-        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        cursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+        );
+
         startManagingCursor(cursor);
 
         // data is a array of String type which is
-        // used to store Number ,Names and id.
-        String[] data = {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone._ID};
+        // used to store Number and Names.
+        String[] data = {
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+
         int[] to = {android.R.id.text1, android.R.id.text2};
 
         // creation of adapter using SimpleCursorAdapter class
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, data, to);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_multiple_choice,
+                cursor,
+                data,
+                to
+        );
 
         // Calling setAdaptor() method to set created adapter
         listView.setAdapter(adapter);
